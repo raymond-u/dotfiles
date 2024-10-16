10a5326bbbcfc661a6ff2d34ab1b4d38759b634b
raymond-u <36328498+raymond-u@users.noreply.github.com>
2024-10-16 17:39:59 +0800

    feat: add rsync

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 
 # Repo
 repo=https://github.com/raymond-u/dotfiles.git
-version='0.10.2'
+version='0.10.3'
 
 # Scripts
 crypto=src/crypto.sh
@@ -89,6 +89,7 @@ brew_pkgs=(
     '  onefetch'
     '  pandoc'
     '  rename'
+    '  rsync'
     '  task'
     '  tealdeer'
     ''
